cce9c10558e355ba9cc959c8ce13fe1bcf153df1
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-09-01 11:13:24 +0200

    feat: read and parse source map and list its files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import { useState } from 'react';
+
+interface SourceMap {
+  version: number;
+  sources: string[];
+  sourcesContent: string[];
+  names: string[];
+  mappings: string;
+}
+
+const emptySourceMap: SourceMap = {
+  version: 3,
+  sources: [],
+  sourcesContent: [],
+  names: [],
+  mappings: '',
+};
+
 function App() {
+  const [sourceMap, setSourceMap] = useState<SourceMap>(emptySourceMap);
+
+  function handleFileChange(e: any) {
+    const input = e.target;
+    if (!input.files) {
+      setSourceMap(emptySourceMap);
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      try {
+        setSourceMap(JSON.parse(reader.result as string));
+      } catch (err) {
+        console.error(err);
+        alert('Failed to parse source map contents');
+      }
+    };
+    reader.readAsText(input.files[0]);
+  }
+
   return (
     <div>
       <h1>Welcome to MapVis</h1>
+
+      <input type="file" onChange={handleFileChange} />
+
+      <ul>
+        {sourceMap.sources.map((file) => (
+          <li>{file}</li>
+        ))}
+      </ul>
     </div>
   );
 }
